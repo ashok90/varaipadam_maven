ca1e505edd6f7c1e0ca7975cd21dc377752d7304
ashok90 <ash4008@gmail.com>
2018-04-24 18:58:45 -0700

    Some other changes

--- data_pull/get_maven_data.py
+++ data_pull/get_maven_data.py
@@ -14,24 +14,25 @@ file = open(EXPORT_path+"link","w")
 
 def extract_dependency(url):
     ##function to load the dependencies in the GLOBAL list DEP_lst
-    global ITER
-    if ITER<=10:
-        print(url)
-        with urllib.request.urlopen(url) as url:
-            r = url.read()
-        soup = BeautifulSoup(r, 'html.parser')
-        title=soup.find("h2", {"class": "im-title"}).findNext("a").get_text().strip(' ')
-        compile_dependencies = soup.find("h2", text=re.compile('Compile Dependen.*'))
-        table = compile_dependencies.findNext("table")
-        rows = table.findAll('tr')
-        
-        for tr in rows:
-            tmp = ""
-            for tag in tr.find_all('a')[-4:]:
-                tmp = tmp + tag.getText().strip('\n') + "|"
-                DEP_lst.append(title+"|"+tmp)
-        ITER+=1
-        flow_cntrl()
+    if urllib.request.urlopen(url):
+        global ITER
+        if ITER<=15:
+            print(url)
+            with urllib.request.urlopen(url) as url:
+                r = url.read()
+            soup = BeautifulSoup(r, 'html.parser')
+            title=soup.find("h2", {"class": "im-title"}).findNext("a").get_text().strip(' ')
+            compile_dependencies = soup.find("h2", text=re.compile('Compile Dependen.*'))
+            table = compile_dependencies.findNext("table")
+            rows = table.findAll('tr')
+
+            for tr in rows:
+                tmp = ""
+                for tag in tr.find_all('a')[-4:]:
+                    tmp = tmp + tag.getText().strip('\n') + "|"
+                    DEP_lst.append(title+"|"+tmp)
+            ITER+=1
+            flow_cntrl()
             
 def flow_cntrl():
     ##function to start the extract_dependency function and write the GLOBAL list DEP_lst to a .csv FILE
@@ -44,7 +45,7 @@ def flow_cntrl():
         extract_dependency(START_url)
     else:
         
-        print("inside"+str(ITER))
+        print("Iteration->"+str(ITER))
         #write the file
         for i in DEP_lst:
             if len(i.split('|'))==5:
@@ -60,27 +61,29 @@ def flow_cntrl():
             s_group_id,s_artifact_id,s_version=j.split('|')[1:4]
             url=build_URL(s_group_id,s_artifact_id,s_version)
             if urllib.request.urlopen(url):
-                time.sleep(5)
+                #time.sleep(5)
                 extract_dependency(url)
+            else:
+                continue
+                
 
 def build_URL(group_id,artifact_id,version):
     urllst=[MAVEN_url,group_id,artifact_id,version]
     return reduce((lambda x,y:x+"/"+y),urllst)
 
 def main():
+    #org.eclipse.jdt|core|3.1.1
     group_id="org.apache.hadoop"
     artifact_id="hadoop-core"
     version="1.2.1"
     global START_url
     global file
+    
     START_url=build_URL(group_id,artifact_id,version)
-    print("Parent URL Parsed : "+START_url)
-    #extract_dependency(FINAL_url,ITER)
-
-    #extract_dependency(FINAL_url)
+    
     flow_cntrl()
     file.close()
-
+    print("Completed")
 
 # Execute main() function
 if __name__ == '__main__':
