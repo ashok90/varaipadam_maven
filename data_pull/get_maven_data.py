b4bafec280d04f54a24e4c8e595b89632916ad3d
brajaram <brajaram@umail.iu.edu>
2018-04-24 23:45:54 -0700

    removed recursion, added q based approach

--- data_pull/get_maven_data.py
+++ data_pull/get_maven_data.py
@@ -3,87 +3,74 @@ import urllib.request
 from functools import reduce
 import re
 import time
-
-EXPORT_path="/Users/ashokkuppuraj/Documents/IndianaUniv/Spring2018/SQL-no-SQL/Final_project/Project/srcdata/"
-MAVEN_url="https://mvnrepository.com/artifact"
-ARTIFACT_id=""
-DEP_lst=[]
-ITER=0
-START_url=""
-file = open(EXPORT_path+"link","w")
+import sys
 
 def extract_dependency(url):
     ##function to load the dependencies in the GLOBAL list DEP_lst
-    if urllib.request.urlopen(url):
-        global ITER
-        if ITER<=15:
-            print(url)
+    l = []
+    try:
+        if urllib.request.urlopen(url):
             with urllib.request.urlopen(url) as url:
                 r = url.read()
-            soup = BeautifulSoup(r, 'html.parser')
-            title=soup.find("h2", {"class": "im-title"}).findNext("a").get_text().strip(' ')
-            compile_dependencies = soup.find("h2", text=re.compile('Compile Dependen.*'))
-            table = compile_dependencies.findNext("table")
-            rows = table.findAll('tr')
-
-            for tr in rows:
-                tmp = ""
-                for tag in tr.find_all('a')[-4:]:
-                    tmp = tmp + tag.getText().strip('\n') + "|"
-                    DEP_lst.append(title+"|"+tmp)
-            ITER+=1
-            flow_cntrl()
-            
-def flow_cntrl():
-    ##function to start the extract_dependency function and write the GLOBAL list DEP_lst to a .csv FILE
-    global DEP_lst
-    global START_url
-    tlst=[]
-   
-    if ITER==0:
-        #First time run the extract dependency function
-        extract_dependency(START_url)
-    else:
-        
-        print("Iteration->"+str(ITER))
-        #write the file
-        for i in DEP_lst:
-            if len(i.split('|'))==5:
-                print(i)
-                #print(i.split('|')[1:4])
-                file.write(i+'\n')
-                tlst.append(i)
-        
-        #print(DEP_lst)
-        DEP_lst=[]
-        #Iterate for the dependent URLs
-        for j in tlst:
-            s_group_id,s_artifact_id,s_version=j.split('|')[1:4]
-            url=build_URL(s_group_id,s_artifact_id,s_version)
-            if urllib.request.urlopen(url):
-                #time.sleep(5)
-                extract_dependency(url)
-            else:
-                continue
+                soup = BeautifulSoup(r, 'html.parser')
+                title=soup.find("h2", {"class": "im-title"}).findNext("a").get_text().strip(' ')
+                compile_dependencies = soup.find("h2", text=re.compile('Compile Dependen.*'))
+                table = compile_dependencies.findNext("table")
+                rows = table.findAll('tr')
+                for tr in rows:
+                    tmp = ""
+                    for tag in tr.find_all('a')[-4:]:
+                        tmp = tmp + tag.getText().strip('\n') + "|"
+                    l.append(title+"|"+tmp)
+        return l
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
                 
 
-def build_URL(group_id,artifact_id,version):
-    urllst=[MAVEN_url,group_id,artifact_id,version]
+def build_url(group_id,artifact_id,version):
+    maven_url="https://mvnrepository.com/artifact"
+    urllst=[maven_url,group_id,artifact_id,version]
     return reduce((lambda x,y:x+"/"+y),urllst)
+
+def get_parser():
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+    parser = ArgumentParser(description=__doc__,
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-g', '--group', dest='group_id')
+    parser.add_argument('-a', '--artifact', dest='artifact_id')
+    parser.add_argument('-v', '--version', dest='version')
+    return parser
+
+def extract_dependency_master(group_id, artifact_id, version):
+    l = []
+    tmp = "|"  + group_id + "|" + artifact_id + "|" + version
+    l.append(tmp)
+    while l:
+        popped = l.pop()
+        print(popped)
+        try:
+            s_group_id,s_artifact_id,s_version=popped.split('|')[1:4]
+            url=build_url(s_group_id,s_artifact_id,s_version)
+            l.extend(extract_dependency(url))
+            print(l)
+        except:
+            pass
 
 def main():
     #org.eclipse.jdt|core|3.1.1
-    group_id="org.apache.hadoop"
-    artifact_id="hadoop-core"
-    version="1.2.1"
-    global START_url
-    global file
-    
-    START_url=build_URL(group_id,artifact_id,version)
-    
-    flow_cntrl()
-    file.close()
-    print("Completed")
+    parser = get_parser()
+    if (len(sys.argv) < 2):
+        parser.print_help()
+    else:
+        args = parser.parse_args()
+        # group_id="org.apache.hadoop"
+        group_id = args.group_id
+        # artifact_id="hadoop-core"
+        artifact_id = args.artifact_id
+        # version="1.2.1"
+        version = args.version
+        extract_dependency_master(group_id,artifact_id,version)
+        print("Completed")
 
 # Execute main() function
 if __name__ == '__main__':
