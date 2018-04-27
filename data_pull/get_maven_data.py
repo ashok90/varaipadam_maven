63a17883a2c38105323a96c58892173710d2613e
ashok90 <ash4008@gmail.com>
2018-04-26 18:50:05 -0700

    Update for file generation

--- data_pull/get_maven_data.py
+++ data_pull/get_maven_data.py
@@ -5,16 +5,25 @@ import re
 import time
 import sys
 
+
+
 def extract_dependency(url):
     ##function to load the dependencies in the GLOBAL list DEP_lst
     l = []
+    URL=str(url)
+    file1 = open("/Users/ashokkuppuraj/Documents/IndianaUniv/Spring2018/SQL-no-SQL/Final_project/Project/srcdata/node.txt","w") 
+    file2 = open("/Users/ashokkuppuraj/Documents/IndianaUniv/Spring2018/SQL-no-SQL/Final_project/Project/srcdata/link.txt","w")
     try:
         if urllib.request.urlopen(url):
             with urllib.request.urlopen(url) as url:
                 r = url.read()
                 soup = BeautifulSoup(r, 'html.parser')
                 title=soup.find("h2", {"class": "im-title"}).findNext("a").get_text().strip(' ')
+                tmp1='|'.join(URL.split('/')[-3:])
                 compile_dependencies = soup.find("h2", text=re.compile('Compile Dependen.*'))
+                ##-----write the file 1 
+                file1.write(title+'|'+tmp1+'|'+re.findall(r'\b\d+\b',compile_dependencies.get_text())[0])
+                file1.close()
                 table = compile_dependencies.findNext("table")
                 rows = table.findAll('tr')
                 for tr in rows:
@@ -23,7 +32,10 @@ def extract_dependency(url):
                         tmp = tmp + tag.getText().strip('\n') + "|"
                     tmp = title + '|' + tmp
                     print(tmp)
+                    ##-----write the file 2
+                    file2.write(tmp+'\n')
                     l.append(tmp)
+        file2.close()
         return l
     except:
         print("Unexpected error:", sys.exc_info()[0])
@@ -54,6 +66,7 @@ def extract_dependency_master(group_id, artifact_id, version):
             s_group_id,s_artifact_id,s_version=popped.split('|')[1:4]
             url=build_url(s_group_id,s_artifact_id,s_version)
             l.extend(extract_dependency(url))
+            #extract_dependency(url)
             #print(l)
         except:
             pass
@@ -64,13 +77,13 @@ def main():
     if (len(sys.argv) < 2):
         parser.print_help()
     else:
-        args = parser.parse_args()
-        # group_id="org.apache.hadoop"
-        group_id = args.group_id
-        # artifact_id="hadoop-core"
-        artifact_id = args.artifact_id
-        # version="1.2.1"
-        version = args.version
+        #args = parser.parse_args()
+        group_id="org.apache.hadoop"
+        #group_id = args.group_id
+        artifact_id="hadoop-core"
+        #artifact_id = args.artifact_id
+        version="1.2.1"
+        #version = args.version
         extract_dependency_master(group_id,artifact_id,version)
         print("Completed")
 
