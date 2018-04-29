e347b400c5e69f150bca727ccf983dbfaa232671
brajaram <brajaram@umail.iu.edu>
2018-04-29 12:32:24 -0700

    draft UI changes

--- dependencyapp/src/Components/Home.js
+++ dependencyapp/src/Components/Home.js
@@ -6,6 +6,12 @@ class Home extends Component {
             <div className="container">
                 <div className="heading"> <h3 align="center">Maven Dependency Management</h3> </div>
                 <div className="row">
+                    <div className="col-md-3"></div>
+                    <div className="col-md-8 img-container">
+                        <img src={require("./data-flow.jpeg")}></img>
+                    </div>
+                </div>
+                {/* <div className="row">
                     <div className="col-md-1"></div>
                     <div className="col-md-3">
                         <div className="tech-top" align="center"><b>Neo4j</b></div>
@@ -14,18 +20,18 @@ class Home extends Component {
                             </div>
                     </div>
                     <div className="col-md-3">
-                        <div className="tech-top" align="center"><b>Neo4j</b></div>
+                        <div className="tech-top" align="center"><b>Eclipse Vert.x</b></div>
                         <div className="tech-stack">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </div>
                     </div>
                     <div className="col-md-3">
-                        <div className="tech-top" align="center"><b>Neo4j</b></div>
+                        <div className="tech-top" align="center"><b>Reactjs</b></div>
                         <div className="tech-stack">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </div>
                     </div>
-                </div>
+                </div> */}
                 <div className="row">
                     <div className="description">
                         <div align="center"><b>Project Description</b></div><br />
